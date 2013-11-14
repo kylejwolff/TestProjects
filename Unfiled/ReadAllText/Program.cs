614fce6648f79369a880da2fa29ee800d4e0cd3b
kylejwolff <molluskcreature@gmail.com>
2013-11-14 12:21:31 -0500

    Attempted to Document ReadAllText

--- Unfiled/ReadAllText/Program.cs
+++ Unfiled/ReadAllText/Program.cs
@@ -7,33 +7,28 @@ using System.Threading.Tasks;
 
 namespace ReadAllText
 {
+    /// <summary>
+    /// Simple Project called <c>ReadAllText</c>
+    /// </summary>
+    /// <remarks>This project prints the text from "Truck.txt" to a Console Window</remarks>
+    
+    /// class Program
     class Program
     {
+        /// <summary>
+        /// The entry point for he application
+        /// </summary>
+        /// <param name="args">A list of command line arguments</param>
         static void Main(string[] args)
         {
-            // Read the file as one string.
-            System.IO.StreamReader myFile =
-               new System.IO.StreamReader("c:\\TestProjects\\Truck.txt");
-            string myString = myFile.ReadToEnd();
-
-            myFile.Close();
-
-            // Display the file contents.
-            Console.WriteLine(myString);
-            // Suspend the screen.
-            Console.ReadLine();
-
-            //correct, more compact version of the above
-            // using statement closes, disposes the stream no matter what
-            // prevents the file from being locked if your app crashes for whatevs reason
-            using (var filestream = new StreamReader(args[0]))
-                Console.WriteLine(filestream.ReadToEnd());
-            Console.ReadLine();
-
-            //How I'd have done it.
-            //The framework is your friend.
+            /// Calls method <c>WriteLine</c> from the <c>Console</c> class,
+            /// which writes "Truck.txt" by calling method <c>ReadAllText</c>
+            /// from the <c>File</c> class which reads "Truck.text" because it
+            /// is the first Argument in the Command line.
             Console.WriteLine(File.ReadAllText(args[0]));
+            /// Prompts user to push Enter to close the Console Window
             Console.WriteLine("Hit enter to exit");
+            /// Closes the Console Window
             Console.ReadLine();
         }
     }
